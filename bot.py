74d2efdd4fb791700951819a4367ccba29d80ef6
mtrj <57919946+mtrj@users.noreply.github.com>
2020-04-01 16:36:39 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -17,42 +17,39 @@ class bbb:
         self.un = un
         self.pw = pw
         self.driver = webdriver.Chrome(self.path, options=chrome_options)
+        self.driver.implicitly_wait(5)
         
     def _login(self):
         """
         Os XPaths utilizados são específicos da votação da Manu 31/3/20
         """
         driver = self.driver
-        driver.implicitly_wait(10)
         link_login = 'https://minhaconta.globo.com'
         driver.get(link_login)
         un, pw = self.un, self.pw
         x_un = '/html/body/div[1]/main/div[2]/div/div/div/div[2]/div[1]/form/div[1]/input'
         x_pw = '/html/body/div[1]/main/div[2]/div/div/div/div[2]/div[1]/form/div[3]/div[1]/input'
         x_final = '/html/body/div[1]/main/div[2]/div/div/div/div[2]/div[1]/form/div[6]/button'
-        un_input = driver.find_element_by_xpath(x_un).send_keys(un)
-        pw_input = driver.find_element_by_xpath(x_pw).send_keys(pw)
-        final_click = driver.find_element_by_xpath(x_final).click()
+        driver.find_element_by_xpath(x_un).send_keys(un)
+        driver.find_element_by_xpath(x_pw).send_keys(pw)
+        driver.find_element_by_xpath(x_final).click()
             
     def _vote(self, votos=10):
         self._login()
         driver = self.driver
         link_votacao = 'https://gshow.globo.com/realities/bbb/bbb20/votacao/paredao-bbb20-quem-voce-quer-eliminar-felipe-manu-ou-mari-a9f49f90-84e2-4c12-a9af-b262e2dd5be4.ghtml'
         driver.get(link_votacao)
-        for i in range(1,votos):
+        for i in range(1,votos+1):
             print('{0}/{1} voto em andamento'.format(i, votos))
             driver.find_element_by_xpath('/html/body/div[2]/div[4]/div/div[1]/div[4]/div[2]/div/div[1]').click()
-            driver.implicitly_wait(15)
             try:
                 driver.find_elements_by_xpath('//*[@id="roulette-root"]/div/div[1]/div[4]/div[2]/div[2]/div/div/div[2]/div/div[2]/img')[0].click()
             except IndexError:
                 driver.get(link_votacao)
                 driver.find_element_by_xpath('/html/body/div[2]/div[4]/div/div[1]/div[4]/div[2]/div/div[1]').click()
-            driver.implicitly_wait(15)
             x_loop = '/html/body/div[2]/div[4]/div/div[3]/div/div/div[1]/div[2]/button'
             votou=False
             while votou==False:
-                time.sleep(5)
                 try:
                     driver.find_elements_by_xpath(x_loop).click()
                     votou = True
@@ -64,7 +61,6 @@ class bbb:
                         driver.get(link_votacao)
                         votou=True
                         break 
-                    time.sleep(10)
-                    driver.implicitly_wait(15)
+                    time.sleep(3)
                     continue
             print('{0}/{1} voto concluído'.format(i, votos))
